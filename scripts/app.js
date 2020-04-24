43ba33226456b7efa1f5681b505bba06a76f2a3d
Chris Kalema <chriskalma01@gmail.com>
2020-04-24 23:31:09 +0300

    initial scripts

---
+++ scripts/app.js
@@ -0,0 +1,76 @@
+const cityForm = document.querySelector('form');  //capture data from the form
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+
+    console.log(data);
+    const cityDetails = data.cityDetails;
+    const weather = data.weather;
+
+    //Alternative = Destructuring properties
+    //const {cityDetails, weather} = data;
+
+    details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    `;
+
+    //update icon images
+
+    const iconsrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconsrc);
+
+    // update might/day images
+    let timesrc = null;
+    if(weather.IsDayTime){
+        timesrc = 'img/day.svg';
+    }else{
+        timesrc = 'img/night.svg';
+    }
+    time.setAttribute('src', timesrc);   //change the card image to day or night
+    //remove the d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+};
+
+const updateCity = async (city)=>{      //async because the methods inside returns promises
+
+    const cityDetails = await getCity(city);     
+    const weather = await getWeather(cityDetails.Key);
+
+    return{
+        cityDetails: cityDetails,
+        weather: weather
+    };
+};
+
+cityForm.addEventListener('submit', e=>{
+    //prevent default value
+    e.preventDefault();
+
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    //update UI with the submitted city
+    updateCity(city)
+    .then(data => updateUI(data))
+    .catch(error => console.log(error));
+
+    //set local storage
+    localStorage.setItem('city', city);
+});
+
+//show the recent city every refresh
+if(localStorage.getItem('city')){
+    updateCity(localStorage.getItem('city'))
+        .then(data =>{ updateUI(data)})
+        .catch(error =>console.log(error));
+}
---
+++ scripts/forecast.js
@@ -0,0 +1,31 @@
+
+const key = '95UcSEjYQGif6xGvtvSCkvrSNDDi9TBu';    //api key from accu-weather
+
+//get weather information
+const getWeather = async (id) =>{
+
+    const base = `http://dataservice.accuweather.com/currentconditions/v1/${id}`;
+    const query = `?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    
+    return data[0];
+;}
+
+//Get City information
+const getCity = async (city) =>{          // async method to get data about a city
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;    //submit in the query - key and name of the city
+
+    const response = await fetch(base + query);
+    const data = await response.json();   
+
+    return data[0];   //return the first city
+};
+
+// getCity('New York')
+//     .then(data => {return getWeather(data.Key);})       //use the getWeather method to get the City Key
+//     .then(data =>{console.log(data);})
+//     .catch(error => console.log(error));              
